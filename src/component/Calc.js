c0c89b75bc1a6ceb6dac45d363951aff22f40396
DaehyeonKim <Daehyeonoff@gmail.com>
2021-10-20 23:46:30 +0900

    20211020

--- src/component/Calc.js
+++ src/component/Calc.js
@@ -1,56 +1,57 @@
-import React, { Component } from 'react';
-import { Fragment } from 'react';
+import React, {Component} from 'react';
+import {Fragment} from 'react';
 
 class Calc extends Component {
 
-state = {
-    num1: 0,
-    num2: 0,
-    result: 0,
-}   
-
-handleOnChange = (e) => {
-   this.setState( {
-   num1: e.target.value
-
-   } )
-}
-
-handleOnChange2 = (e) => {
-    this.setState( {
-    num2: e.target.value
- 
-    } )
- }
-
-handleClick = () => {
-  this.setState( {
-    result: Number(this.state.num1)*Number (this.state.num2)
-
-  } )
-
-}
-
-render() {
+    state = {
+        num1: 0,
+        num2: 0,
+        result: 0
+    } 
 
 
-    return(
-        <Fragment>
-        <input type="text" onChange={this.handleOnChange}></input>
-        <h1> {this.state.num1} </h1>
-        <input type="text"  onChange={this.handleOnChange2}></input>
-        <h1> {this.state.num2} </h1>
-        <button onClick={this.handleClick}>계산</button>
-        <h1> Result:{this.state.result} </h1>
-        </Fragment>
-    )
-}
+    handleOnChange = (e) => {
+      if (e.key === 'Enter') {
+        this.setState({num1: e.target.value})
+    } 
+    }
 
+    handleOnChange2 = (e) => {
+       
+        if (e.key === 'Enter') {
+            this.setState({num2: e.target.value} )
+        } 
 
+        
+    }
 
+    handleClick = () => {
+        this.setState({
+            result: Number(this.state.num1) ** Number(this.state.num2)
 
- 
+        })
 
+    }
+
+    render() {
+
+        return (
+            <Fragment>
+                <input type="text" onKeyPress={this.handleOnChange}></input>
+                <h1>
+                    {this.state.num1}
+                </h1>
+                <input type="text" onKeyPress={this.handleOnChange2}></input>
+                <h1>
+                    {this.state.num2}
+                </h1>
+                <button onClick={this.handleClick}>계산</button>
+                <h1>
+                    Result:{this.state.result}
+                </h1>
+            </Fragment>
+        )
+    }
 
 }
 
